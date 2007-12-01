bbcb1e471897893e23b7e7ad652c6601ff24b441
AstroFloyd <AstroFloyd@gmail.com>
2007-12-01 13:00:00 +0100

    Tracking old changes:  version of 01/12/2007
    Cleaned up old tools in tools/source/

--- tools/source/hms2rad.f
+++
@@ -1,8 +0,0 @@
-      use constants
-      implicit none
-      real*8 :: o,h,m,s,hms2rad
-      call declconst
-      write(6,'(A18,$)')'Input RA (h,m,s): '
-      read*,h,m,s
-      write(6,'(30x,F13.10)')hms2rad(h,m,s)
-      end
