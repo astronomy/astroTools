b679e4f6f85cee2e9ad47d25f4ce3d85fec187c4
AstroFloyd <AstroFloyd@gmail.com>
2012-08-29 19:49:52 +0200

    Updated functions.f90 in tools:
    * Updated functions.f90 in tools:
      * copied source_old/functions.f to src/functions.f90
      * allow compilation with gfortran

--- tools/src/rect2spher.f90
+++ tools/src/rect2spher.f90
@@ -1,3 +1,8 @@
+!> \file rect2spher  Convert rectangular coordinates to spherical coordinates
+
+!***********************************************************************************************************************************
+!> \brief  Convert rectangular coordinates to spherical coordinates
+
 program rect2spher
   use SUFR_kinds, only: double
   use SUFR_system, only: quit_program
@@ -14,20 +19,21 @@ program rect2spher
        call quit_program('syntax: rect2spher <x> <y> <z>')
   
   call get_command_argument(1,str)
-  read(str,*)x
+  read(str,*) x
   call get_command_argument(2,str)
-  read(str,*)y
+  read(str,*) y
   call get_command_argument(3,str)
-  read(str,*)z
+  read(str,*) z
   
   call rect_2_spher(x,y,z, l,b,r)
   
   write(*,*)
-  write(6,'(A,3F20.10)')'x,y,z: ',x,y,z
-  write(6,'(A,3F20.10)')'l,b,r: ',rev(l)*r2d,rev2(b)*r2d,r
+  write(6,'(A,3F20.10)') 'x,y,z: ', x, y, z
+  write(6,'(A,3F20.10)') 'l,b,r: ', rev(l)*r2d, rev2(b)*r2d, r
   write(*,*)
   
 end program rect2spher
+!***********************************************************************************************************************************
 
 
 
