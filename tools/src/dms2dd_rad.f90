4c7c2c795e8c2c91cacbdca9ed466e463f696fde
AstroFloyd <AstroFloyd@gmail.com>
2015-02-14 11:58:36 +0100

    Added program dms2dd_rad
    * Added program dms2dd_rad:
      * converts an angle expressed in degrees, arcminutes and arcseconds to decimal degrees
        and radians
      * moved from source_old_old

--- tools/src/dms2dd_rad.f90
+++ tools/src/dms2dd_rad.f90
@@ -1,12 +1,10 @@
-!> \file  dd2dms_rad.f90  Convert an angle expressd in decimal degrees to  degrees and arcminutes,
-!!                        degrees, arcminutes and arcseconds,  and  radians
+!> \file program dms2dd_rad.f90  Convert an angle expressed in degrees, arcminutes and arcseconds to decimal degrees and radians
 
 
 !***********************************************************************************************************************************
-!> \brief  Convert an angle expressd in decimal degrees to  degrees and arcminutes,  degrees, arcminutes and arcseconds,
-!!         and  radians
+!> \brief  Convert an angle expressed in degrees, arcminutes and arcseconds to decimal degrees and radians
 
-program dd2dms_rad
+program dms2dd_rad
   use SUFR_kinds, only: double
   use SUFR_constants, only: set_SUFR_constants, d2r
   use SUFR_angle2string, only: dmss2
@@ -14,17 +12,28 @@ program dd2dms_rad
   use SUFR_command_line, only: get_command_argument_d
   
   implicit none
-  real(double) :: angle
+  real(double) :: d,m,s, angle
   character :: dmmmmm2*(12)
   
   call set_SUFR_constants()
   
-  if(command_argument_count().ne.1)  call syntax_quit('<angle>',0, &
-       'dd2dms_rad converts an angle expressed in decimal degrees to degrees, arcminutes and arcseconds, and radians')
-     
-  call get_command_argument_d(1, angle)
+  if(command_argument_count().ne.3)  call syntax_quit('<degrees> <arcminutes> <arcseconds>',0, &
+       'dms2dd_rad converts an angle expressed in degrees, arcminutes and arcseconds to decimal degrees and radians')
   
-  !write(*,'(2x,F14.7,A,A15,A16, F20.12,A)') angle,' d', dmmmmm2(angle*d2r), dmss2(angle*d2r), angle*d2r,' rad'
+  !write(6,'(A22,$)') 'Input angle (d,m,s): '
+  !read*,d,m,s
+  
+  call get_command_argument_d(1, d)
+  call get_command_argument_d(2, m)
+  call get_command_argument_d(3, s)
+  
+  if(d.lt.0.d0) then
+     m = -dabs(m)
+     s = -dabs(s)
+  end if
+  
+  angle = d + m/60.d0 + s/3600.d0
+  
   write(*,*)
   write(*,*) '  Decimal degrees:                             ',angle
   write(*,*) '  Degrees and decimal arcminutes:                ',dmmmmm2(angle*d2r)
@@ -32,6 +41,5 @@ program dd2dms_rad
   write(*,*) '  Radians:                                     ',angle*d2r, '  =  ',angle/180.d0,' pi'
   write(*,*)
   
-end program dd2dms_rad
+end program dms2dd_rad
 !***********************************************************************************************************************************
-
