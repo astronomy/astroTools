7300a3b42a3efa5b05f2589cc17e11ef36ff07bf
AstroFloyd <AstroFloyd@gmail.com>
2015-05-10 17:34:10 +0200

    Added cal2gps

--- src/jd2cal.f90
+++ src/jd2cal.f90
@@ -6,9 +6,10 @@
 
 program jd2calendar
   use SUFR_kinds, only: double
+  use SUFR_constants, only: endays
   use SUFR_system, only: syntax_quit
   use SUFR_command_line, only: get_command_argument_d
-  use SUFR_date_and_time, only: jd2cal
+  use SUFR_date_and_time, only: jd2cal, dow_ut
   use SUFR_time2string, only: hms_sss
   use TheSky_datetime, only: calc_gmst
   
@@ -26,7 +27,7 @@ program jd2calendar
   call jd2cal(jd,y,m,d)
   
   write(*,'(/,A,F20.7)')    '    JD:     ',jd
-  write(*,'(A,I9,I3,F7.3)') '    Date:   ',y,m,d
+  write(*,'(A,5x,A,I7,I3,F7.3)') '    Date:   ',trim(endays(dow_ut(jd))),y,m,d
   write(*,'(A,5x,A)')       '    UT:     ',hms_sss((d-floor(d))*24)
   write(*,'(A,5x,A,/)')     '    GMST:   ',hms_sss(calc_gmst(jd))
   
